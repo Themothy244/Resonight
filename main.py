3ef3992bb2641059d32953076c8ce6c3933bd13d
Admin <nerakbangtan@gmail.com>
2026-04-20 23:40:44 +0800

    0o0

--- main.py
+++ main.py
@@ -54,6 +54,8 @@ class Game:
                 Platform(100, 650, 250, 20),
                 Platform(400, 580, 200, 20),
                 Platform(650, 500, 180, 20),
+                Platform(650, 500, 180, 20),
+
             ],
             spikes=[
                 Spike(420, 650, 30, 30),
