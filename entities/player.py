c9194af50b166b6eb9e7c6f373a584c89c886c55
Themothy James <themothyjamesmaloma@gmail.com>
2026-04-29 21:00:38 +0800

    added tap jump and hold jump

--- entities/player.py
+++ entities/player.py
@@ -7,7 +7,9 @@ class Player:
         self.speed = 5
         self.y_velocity = 0
         self.gravity = 0.5
+        self.low_jump_multiplier = 2
         self.jump_strength = 13
+        self.is_jumping = False
 
         self.current_platform = None
         self.on_platform = False
@@ -62,9 +64,16 @@ class Player:
             dx = self.speed
 
         if keys[pygame.K_SPACE] and self.on_ground_or_platform(platforms, ground_y):
-            self.y_velocity = -self.jump_strength
+                self.y_velocity = -self.jump_strength
 
-        self.y_velocity += self.gravity
+        if self.y_velocity < 0:
+            if not keys[pygame.K_SPACE]:
+                self.y_velocity += self.gravity * self.low_jump_multiplier
+            else:
+                self.y_velocity += self.gravity
+        else:
+            self.y_velocity += self.gravity
+
         self.y_velocity = self.move_and_collide(platforms, dx, self.y_velocity)
 
         if self.on_platform and self.current_platform:
